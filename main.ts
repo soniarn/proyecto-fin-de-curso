39db0673d2f961218e90162f814794a9d5837b94
soniarn <130452815+soniarn@users.noreply.github.com>
2023-06-13 12:17:21 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,34 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showNumber(lectura)
+    basic.pause(100)
+    basic.showNumber(lectura)
+    basic.pause(100)
+    basic.showNumber(lectura)
+    basic.pause(100)
+})
+radio.onReceivedString(function (receivedString) {
+    radio.setGroup(1)
+})
 let lectura = 0
+radio.setGroup(19)
 basic.forever(function () {
-    lectura = pins.analogReadPin(AnalogPin.P0)
-    led.plotBarGraph(
-    lectura,
-    1023
-    )
     if (input.buttonIsPressed(Button.A)) {
+        basic.showNumber(input.temperature())
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
+        basic.showNumber(input.temperature())
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        lectura = pins.digitalReadPin(DigitalPin.P0)
         basic.clearScreen()
         basic.pause(100)
-        basic.showNumber(lectura)
+        basic.showNumber(0)
+    }
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showNumber(input.temperature())
     }
 })
